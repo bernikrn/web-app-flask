99334c41bc1f4fd1b3e9bdaabafc9c81490f175a
Bernardo Koron <bernardokoron@gmail.com>
2022-12-03 10:45:09 +0100

    Sign up form validation

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, render_template, request, redirect, url_for
+from forms import SignupForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
 
 posts = []
-
 
 @app.route("/")
 def index():
@@ -22,4 +24,13 @@ def post_form(post_id=None):
 
 @app.route("/signup/", methods=["GET", "POST"])
 def show_signup_form():
-    return render_template("signup_form.html")
+    form = SignupForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('index'))
+    return render_template("signup_form.html", form=form)
